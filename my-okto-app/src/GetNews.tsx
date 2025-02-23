2e9986a3ba7839675b378e472e4735f61e758ea3
nishanthspk <spknishanth2004@gmail.com>
2025-02-23 22:07:42 +0530

    format

--- my-okto-app/src/GetNews.tsx
+++ my-okto-app/src/GetNews.tsx
@@ -5,93 +5,94 @@ import { encodeFunctionData } from 'viem';
 
 
 
-export default function GetNews({}) {
-  const oktoClient = useOkto();
+export default function GetNews({ }) {
+    const oktoClient = useOkto();
 
 
 
-  const getNews = async () => {
-    try {
-      // 1. Define Contract Interaction
-      const contractAddress = '0xeb0A42C64417114aDbb74f454110452eb0F3292e'; // Replace with actual contract address
-      const functionName = 'getNews'; // Replace with actual function name
-      const functionArgs = [20]; // Pass input value as argument
+    const getNews = async () => {
+        try {
+            // 1. Define Contract Interaction
+            const contractAddress = '0xeb0A42C64417114aDbb74f454110452eb0F3292e'; // Replace with actual contract address
+            const functionName = 'getNews'; // Replace with actual function name
+            const functionArgs = [20]; // Pass input value as argument
 
-      const functionData = encodeFunctionData({
-        abi:[
-            {
-                "inputs": [
+            const functionData = encodeFunctionData({
+                abi: [
                     {
-                        "internalType": "uint256",
-                        "name": "_id",
-                        "type": "uint256"
-                    }
-                ],
-                "name": "getNews",
-                "outputs": [
-                    {
-                        "components": [
+                        "inputs": [
                             {
                                 "internalType": "uint256",
-                                "name": "id",
-                                "type": "uint256"
-                            },
-                            {
-                                "internalType": "address",
-                                "name": "author",
-                                "type": "address"
-                            },
-                            {
-                                "internalType": "string",
-                                "name": "content",
-                                "type": "string"
-                            },
-                            {
-                                "internalType": "uint256",
-                                "name": "timestamp",
+                                "name": "_id",
                                 "type": "uint256"
                             }
                         ],
-                        "internalType": "struct TwitterNews.NewsPost",
-                        "name": "",
-                        "type": "tuple"
-                    }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-            },]as const,
-        functionName,
-        args: functionArgs,
-      });
+                        "name": "getNews",
+                        "outputs": [
+                            {
+                                "components": [
+                                    {
+                                        "internalType": "uint256",
+                                        "name": "id",
+                                        "type": "uint256"
+                                    },
+                                    {
+                                        "internalType": "address",
+                                        "name": "author",
+                                        "type": "address"
+                                    },
+                                    {
+                                        "internalType": "string",
+                                        "name": "content",
+                                        "type": "string"
+                                    },
+                                    {
+                                        "internalType": "uint256",
+                                        "name": "timestamp",
+                                        "type": "uint256"
+                                    }
+                                ],
+                                "internalType": "struct TwitterNews.NewsPost",
+                                "name": "",
+                                "type": "tuple"
+                            }
+                        ],
+                        "stateMutability": "view",
+                        "type": "function"
+                    },] as const,
+                functionName,
+                args: functionArgs,
+            });
 
-      const rawTxParams = {
-        caip2Id: 'eip155:84532', // Specify target chain
-        transaction: {
-          from: "0xa4Ba62BA94EE81898aDE0276283BADdDD174C154",
-          to: contractAddress as '0x${string}',
-          data: functionData,
-        },};
+            const rawTxParams = {
+                caip2Id: 'eip155:84532', // Specify target chain
+                transaction: {
+                    from: "0xa4Ba62BA94EE81898aDE0276283BADdDD174C154",
+                    to: contractAddress as '0x${string}',
+                    data: functionData,
+                },
+            };
 
-        console.log("Function Data:", functionData);
-        console.log("Raw Transaction Params:", rawTxParams);
+            console.log("Function Data:", functionData);
+            console.log("Raw Transaction Params:", rawTxParams);
 
-const result = await evmRawTransaction(oktoClient, rawTxParams);
-    }
-  catch (error) {
-      console.error("Transaction error:", error);
-    }
-  };
+            const result = await evmRawTransaction(oktoClient, rawTxParams);
+        }
+        catch (error) {
+            console.error("Transaction error:", error);
+        }
+    };
 
 
-  return (
+    return (
 
-<div className="mt-4">
-              
-<button onClick={getNews}className="w-full my-4 py-3 px-4 bg-gradient-to-r from-blue-300 to-blue-600 text-white rounded-full hover:from-gray-300 hover:to-gray-500 transition mb-2 font-bold shadow-md">
+        <div className="mt-4">
+
+            <button onClick={getNews} className="w-full my-4 py-3 px-4 bg-gradient-to-r from-blue-300 to-blue-600 text-white rounded-full hover:from-gray-300 hover:to-gray-500 transition mb-2 font-bold shadow-md">
                 Get News
-              </button>
-            </div>
-     );
+            </button>
+        </div>
+    );
 };
 
 
