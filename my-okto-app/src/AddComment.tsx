5525b42578fa460355e078dd4983700f5c878ddb
nishanthspk <spknishanth2004@gmail.com>
2025-02-23 22:04:29 +0530

    result

--- my-okto-app/src/AddComment.tsx
+++ my-okto-app/src/AddComment.tsx
@@ -6,7 +6,7 @@ import { encodeFunctionData } from 'viem';
 export default function addComments({}) {
   const oktoClient = useOkto();
 
-  // State to manage input value and transaction result
+ 
   const [inputValue, setInputValue] = useState<number>();
   const [transactionResult, setTransactionResult] = useState<string | null>(null);
   const [newsContent, setNewsContent] = useState<string>("");
